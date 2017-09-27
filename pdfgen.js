e0ffd0676ad6fa89edefe7e2c26a6f904df06044
kaelanb <kbarbolina@protonmail.com>
2017-09-27 16:59:07 -0400

    update to script

--- pdfgen.js
+++ pdfgen.js
@@ -1,21 +1,38 @@
+#! /usr/local/bin/node
+
 'use strict';
 
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-var link = process.argv[2];
-var fname = process.argv[3];
-console.log(link);
+const url = process.argv[2];
+const fname = process.argv[3];
+
+if(url === undefined || fname === undefined){
+	console.log("Incorrect usage/format. Correct format is:\n npm run pdfgen https://www.google.com <filename>");
+	process.exit(1);
+}
+
+const dir = './pdfs';
+
+if(!fs.existsSync(dir)){
+	console.log(`Creating folder ${dir}...`);
+    fs.mkdirSync(dir);
+    console.log('Done');
+}
 
 (async() => {
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto(link, {waitUntil: 'networkidle'});
-
-await page.pdf({
-  path: fname + '.pdf',
-  format: 'letter'
-});
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, {waitUntil: 'networkidle'});
+	console.log("generating pdf for " + url);
+	
+	await page.pdf({
+	  	path: 'pdfs/' + fname + '.pdf',
+	 	format: 'letter'
+	});
+	console.log(fname + ".pdf has been created inside folder:" + dir);
 
 browser.close();
 
